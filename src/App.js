175f84f7b1b0f94ae4f964603a7d179faff4a37e
mexean <wmexean@gmail.com>
2020-02-04 17:42:39 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch, withRouter } from "react-router-dom";
+import Login from "./components/login/login";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="p-3 mb-2"
+        style={{ height: "100vh", background: "#123456" }}
+      >
+        <div>
+
+          <Switch>
+            <Route exact path="/" component={withRouter(Login)} />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
